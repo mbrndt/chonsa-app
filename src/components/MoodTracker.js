a3aea8cbf60f5985225f7affc4374000741d1225
Mareike <mareike.brandt@hotmail.de>
2022-03-12 00:12:24 +0900

    polished code in heartbuttons

--- src/components/MoodTracker.js
+++ src/components/MoodTracker.js
@@ -4,28 +4,13 @@ import toast from "react-hot-toast";
 import { db } from "../firebase";
 import { Heartbutton } from "../pages/assets/Heartbutton";
 import moment from "moment";
-import { useContext } from "react";
-import { ButtonNameContext } from "../context/ButtonNameContext";
 import "../styles/Tracker.css";
 
 export const MoodTracker = () => {
-  const setButtonName = ButtonNameContext;
-
   const [currentMood, setcurrentMood] = useState("");
-
-  const [button1Pushed, setButton1Pushed] = useState(false);
-  const [button2Pushed, setButton2Pushed] = useState(false);
-  const [button3Pushed, setButton3Pushed] = useState(false);
-  const [button4Pushed, setButton4Pushed] = useState(false);
-  const [button5Pushed, setButton5Pushed] = useState(false);
 
   const moodCollectionRef = collection(db, "moodTracker");
   const currentDateTime = moment().unix();
-
-  var midnight = new Date();
-  midnight.setHours(24, 0, 0, 0); //midnignt
-
-  var timeTilMidnight = midnight.getTime() - Date.now();
 
   const saveMood = () => {
     addDoc(moodCollectionRef, {
@@ -40,25 +25,23 @@ export const MoodTracker = () => {
       <div className="trackerbox heartbutton">
         <button className="main">
           <Heartbutton
+            className={{ currentMood }}
             disabled
-            styles={{
-              backgroundColour: currentMood === "happy" ? `#d6d6d6` : "#8C7284",
-            }}
+            // styles={{
+            //   backgroundColour: { currentMood } === "happy" ? `black` : "white",
+            // }}
           ></Heartbutton>
-          today
+          <p>today:</p>
+          {currentMood}
         </button>
 
         <div className="trackerbox moodselect">
           <button className="main">
             <Heartbutton
               className="happy"
-              disabled={button1Pushed}
               onClick={() => {
-                setButton1Pushed(true);
                 setcurrentMood("happy");
                 saveMood();
-                setTimeout(setButton1Pushed, timeTilMidnight);
-                setButtonName("happy");
               }}
             />
             happy
@@ -67,12 +50,9 @@ export const MoodTracker = () => {
           <button className="main">
             <Heartbutton
               className="sad"
-              disabled={button2Pushed}
               onClick={() => {
-                setButton2Pushed(true);
                 setcurrentMood("sad");
                 saveMood();
-                setTimeout(setButton2Pushed, timeTilMidnight);
               }}
             ></Heartbutton>
             sad
@@ -81,12 +61,9 @@ export const MoodTracker = () => {
           <button className="main">
             <Heartbutton
               className="grumpy"
-              disabled={button3Pushed}
               onClick={() => {
-                setButton3Pushed(true);
-                setcurrentMood("emotion3");
+                setcurrentMood("grumpy");
                 saveMood();
-                setTimeout(setButton3Pushed, timeTilMidnight);
               }}
             />
             grumpy
@@ -95,12 +72,9 @@ export const MoodTracker = () => {
             <Heartbutton
               label="happy"
               className="neutral"
-              disabled={button4Pushed}
               onClick={() => {
-                setButton4Pushed(true);
-                setcurrentMood("emotion4");
+                setcurrentMood("neutral");
                 saveMood();
-                setTimeout(setButton4Pushed, timeTilMidnight);
               }}
             />
             neutral
@@ -109,12 +83,9 @@ export const MoodTracker = () => {
           <button className="main">
             <Heartbutton
               className="exited"
-              disabled={button5Pushed}
               onClick={() => {
-                setButton5Pushed(true);
-                setcurrentMood("emotion5");
+                setcurrentMood("exited");
                 saveMood();
-                setTimeout(setButton5Pushed, timeTilMidnight);
               }}
             />
             excited
