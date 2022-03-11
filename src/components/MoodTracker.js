edb13156b9311f5f52e3bac9b3b7f3118225ceb0
Mareike <mareike.brandt@hotmail.de>
2022-03-11 23:16:52 +0900

    added labels to heart buttons

--- src/components/MoodTracker.js
+++ src/components/MoodTracker.js
@@ -38,74 +38,87 @@ export const MoodTracker = () => {
   return (
     <div className="trackerbox buttons">
       <div className="trackerbox heartbutton">
-        <Heartbutton
-          disabled
-          styles={{
-            backgroundColour: currentMood === "happy" ? `#d6d6d6` : "#8C7284",
-          }}
-        >
+        <button className="main">
+          <Heartbutton
+            disabled
+            styles={{
+              backgroundColour: currentMood === "happy" ? `#d6d6d6` : "#8C7284",
+            }}
+          ></Heartbutton>
           today
-        </Heartbutton>
+        </button>
 
         <div className="trackerbox moodselect">
-          <Heartbutton
-            className="happy"
-            disabled={button1Pushed}
-            onClick={() => {
-              setButton1Pushed(true);
-              setcurrentMood("happy");
-              saveMood();
-              setTimeout(setButton1Pushed, timeTilMidnight);
-              setButtonName("happy");
-            }}
-          />
+          <button className="main">
+            <Heartbutton
+              className="happy"
+              disabled={button1Pushed}
+              onClick={() => {
+                setButton1Pushed(true);
+                setcurrentMood("happy");
+                saveMood();
+                setTimeout(setButton1Pushed, timeTilMidnight);
+                setButtonName("happy");
+              }}
+            />
+            happy
+          </button>
 
-          <Heartbutton
-            className="sad"
-            disabled={button2Pushed}
-            onClick={() => {
-              setButton2Pushed(true);
-              setcurrentMood("sad");
-              saveMood();
-              setTimeout(setButton2Pushed, timeTilMidnight);
-            }}
-          >
-            today
-          </Heartbutton>
+          <button className="main">
+            <Heartbutton
+              className="sad"
+              disabled={button2Pushed}
+              onClick={() => {
+                setButton2Pushed(true);
+                setcurrentMood("sad");
+                saveMood();
+                setTimeout(setButton2Pushed, timeTilMidnight);
+              }}
+            ></Heartbutton>
+            sad
+          </button>
 
-          <Heartbutton
-            className="grumpy"
-            disabled={button3Pushed}
-            onClick={() => {
-              setButton3Pushed(true);
-              setcurrentMood("emotion3");
-              saveMood();
-              setTimeout(setButton3Pushed, timeTilMidnight);
-            }}
-          />
+          <button className="main">
+            <Heartbutton
+              className="grumpy"
+              disabled={button3Pushed}
+              onClick={() => {
+                setButton3Pushed(true);
+                setcurrentMood("emotion3");
+                saveMood();
+                setTimeout(setButton3Pushed, timeTilMidnight);
+              }}
+            />
+            grumpy
+          </button>
+          <button className="main">
+            <Heartbutton
+              label="happy"
+              className="neutral"
+              disabled={button4Pushed}
+              onClick={() => {
+                setButton4Pushed(true);
+                setcurrentMood("emotion4");
+                saveMood();
+                setTimeout(setButton4Pushed, timeTilMidnight);
+              }}
+            />
+            neutral
+          </button>
 
-          <Heartbutton
-            label="happy"
-            className="neutral"
-            disabled={button4Pushed}
-            onClick={() => {
-              setButton4Pushed(true);
-              setcurrentMood("emotion4");
-              saveMood();
-              setTimeout(setButton4Pushed, timeTilMidnight);
-            }}
-          />
-
-          <Heartbutton
-            className="exited"
-            disabled={button5Pushed}
-            onClick={() => {
-              setButton5Pushed(true);
-              setcurrentMood("emotion5");
-              saveMood();
-              setTimeout(setButton5Pushed, timeTilMidnight);
-            }}
-          />
+          <button className="main">
+            <Heartbutton
+              className="exited"
+              disabled={button5Pushed}
+              onClick={() => {
+                setButton5Pushed(true);
+                setcurrentMood("emotion5");
+                saveMood();
+                setTimeout(setButton5Pushed, timeTilMidnight);
+              }}
+            />
+            excited
+          </button>
         </div>
       </div>
     </div>
