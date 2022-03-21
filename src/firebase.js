5afa91c1c32f79390d63c5807d8120bed8769078
Mareike <mareike.brandt@hotmail.de>
2022-03-22 02:08:54 +0900

    added env for firebase

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "@firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA94A-V1M_v_-8_YUQt1wKGRsrVf-d4sdM",
-  authDomain: "chonsa-fa80a.firebaseapp.com",
-  projectId: "chonsa-fa80a",
-  storageBucket: "chonsa-fa80a.appspot.com",
-  messagingSenderId: "959581817428",
-  appId: "1:959581817428:web:d5f6898980d328cff6bbc1",
-  measurementId: "G-L3W88YZZX3",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
